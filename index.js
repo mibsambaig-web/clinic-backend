95ce25bba4d3c64a4fad14d3bc408f5f085426a6
Mibsam Baig <bismillahenterprises@Mibsams-MacBook-Air.local>
2026-05-16 13:23:36 +0500

    use DATABASE_URL for railway postgres

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ app.use(cors())
 app.use(express.json())
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL || null,
+    user: process.env.DATABASE_URL ? undefined : process.env.DB_USER,
+    host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST,
+    database: process.env.DATABASE_URL ? undefined : process.env.DB_NAME,
+    password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD,
+    port: process.env.DATABASE_URL ? undefined : process.env.DB_PORT,
+    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 })
 
 app.get('/', (req, res) => {
